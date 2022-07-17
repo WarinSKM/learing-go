028fefeecb7452a8c86c796d189cccf0823c29e7
warinSKM <warin.jark@gmail.com>
2022-07-17 22:14:25 +0700

    fix wrong print

--- ninja-04/02/main.go
+++ ninja-04/02/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for v := range arr {
-		fmt.Println(v)
+	for i, v := range arr {
+		fmt.Println(i, v)
 	}
 	fmt.Printf("%T", arr)
 }
